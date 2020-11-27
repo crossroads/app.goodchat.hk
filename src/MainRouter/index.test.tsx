6cb8a00931fbae881a073b2e6cb086e8c79ff9b3
Naveed Elahi <statix231@hotmail.com>
2020-11-27 16:18:31 +0800

    GCW-3432 Use Router instead of IonReactRouter
    Components do not show up as expected on JSDOM with IonReactRouter

--- src/MainRouter/index.test.tsx
+++ src/MainRouter/index.test.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import MainRouter from ".";
-import { IonReactMemoryRouter } from "@ionic/react-router";
 import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 
 describe("User visits /home", () => {
   it("should take user to the appropriate URL", () => {
     const history = createMemoryHistory({ initialEntries: ["/home"] });
     render(
-      <IonReactMemoryRouter history={history}>
+      <Router history={history}>
         <MainRouter />
-      </IonReactMemoryRouter>
+      </Router>
     );
 
     expect(history.location.pathname).toEqual("/home");
