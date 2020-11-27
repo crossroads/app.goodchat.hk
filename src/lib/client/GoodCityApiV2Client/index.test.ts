cf7bcf4df437d2e6047da0a4232dead38a5a0900
Naveed Elahi <statix231@hotmail.com>
2020-11-27 10:33:08 +0800

    GCW-3417 Refactor tests cleanup logic

--- src/lib/client/GoodCityApiV2Client/index.test.ts
+++ src/lib/client/GoodCityApiV2Client/index.test.ts
@@ -3,9 +3,12 @@ import GoodCityApiV2Client from "./index";
 import sendPinResponse from "./__mocks__/sendPinResponse.js";
 
 describe("sendPin", () => {
+  let spy: jest.SpyInstance;
+  afterEach(() => spy.mockRestore());
+
   it(`should call axios with the correct URL, headers, params, etc`, () => {
     const SEND_PIN_URL = `${process.env.REACT_APP_API_V2_URL}/auth/send_pin`;
-    const spy = jest.spyOn(axios, "post");
+    spy = jest.spyOn(axios, "post");
     const mobile = "+85262345678";
 
     GoodCityApiV2Client.sendPin(mobile);
@@ -17,19 +20,15 @@ describe("sendPin", () => {
         headers: { "Content-Type": "application/json" },
       }
     );
-
-    spy.mockRestore();
   });
 
   describe("Successful API response", () => {
     it("it should return the appropriate response", async () => {
-      const spy = jest.spyOn(axios, "post").mockResolvedValue(sendPinResponse);
+      spy = jest.spyOn(axios, "post").mockResolvedValue(sendPinResponse);
 
       const data = await GoodCityApiV2Client.sendPin("+85262345678");
 
       expect(data).toEqual(sendPinResponse);
-
-      spy.mockRestore();
     });
   });
 });
