9e852ca88809a0494e9a1cf510416a0e8c2ad77a
Naveed Elahi <statix231@hotmail.com>
2020-11-28 12:42:19 +0800

    GCW-3432 Remove test in incorrect location
    Test belongs to MainRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders without crashing", () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
-});
-
-test("renders an ion-router-outlet", () => {
-  render(<App />);
-  expect(screen.getByTestId("main-router-outlet")).toBeInTheDocument();
 });
