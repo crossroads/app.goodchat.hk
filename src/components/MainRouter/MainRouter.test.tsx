db9ef5eb2a12e8fb77b3f747b3475b115be5530e
Naveed Elahi <statix231@hotmail.com>
2020-12-04 11:35:09 +0800

    GCW-3432 Refactor tests
    Change function signature of render function to allow for currying

--- src/components/MainRouter/MainRouter.test.tsx
+++ src/components/MainRouter/MainRouter.test.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import MainRouter from "./MainRouter";
-import { createMemoryHistory, MemoryHistory } from "history";
+import { createMemoryHistory } from "history";
 import { Router } from "react-router";
 import AuthProvider from "../../components/AuthProvider";
 
-const renderComponent = (initialAuthState: boolean, history: MemoryHistory) => {
-  return render(
-    <AuthProvider initialAuthState={initialAuthState}>
-      <Router history={history}>
-        <MainRouter />
-      </Router>
-    </AuthProvider>
-  );
+const renderComponent = (initialAuthState: boolean) => (
+  initialPath: string
+) => {
+  const history = createMemoryHistory({ initialEntries: [initialPath] });
+  return {
+    history,
+    ...render(
+      <AuthProvider initialAuthState={initialAuthState}>
+        <Router history={history}>
+          <MainRouter />
+        </Router>
+      </AuthProvider>
+    ),
+  };
 };
 
 // TODO extend jest matchers instead
@@ -30,58 +36,50 @@ const expectToBeOnPage = (
 };
 
 describe("Unauthenticated User", () => {
+  const renderUnauthenticatedComponent = renderComponent(false);
+
   describe("visits /home", () => {
     it("should redirect user to Login page with /login as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/home"] });
-      const { container } = renderComponent(false, history);
-
+      const { container, history } = renderUnauthenticatedComponent("/home");
       expectToBeOnPage(container, history.location.pathname, "login");
     });
   });
 
   describe("visits /login", () => {
     it("should take user to Login page with /login as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/login"] });
-      const { container } = renderComponent(false, history);
-
+      const { container, history } = renderUnauthenticatedComponent("/login");
       expectToBeOnPage(container, history.location.pathname, "login");
     });
   });
 
   describe("visits /", () => {
     it("should take user to Login page with /login as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/"] });
-      const { container } = renderComponent(false, history);
-
+      const { container, history } = renderUnauthenticatedComponent("/");
       expectToBeOnPage(container, history.location.pathname, "login");
     });
   });
 });
 
 describe("Authenticated User", () => {
+  const renderAuthenticatedComponent = renderComponent(true);
+
   describe("visits /home", () => {
     it("should take user to Home page with /home as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/home"] });
-      const { container } = renderComponent(true, history);
-
+      const { container, history } = renderAuthenticatedComponent("/home");
       expectToBeOnPage(container, history.location.pathname, "home");
     });
   });
 
   describe("visits /login", () => {
     it("should take user to Login page with /login as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/login"] });
-      const { container } = renderComponent(true, history);
-
+      const { container, history } = renderAuthenticatedComponent("/login");
       expectToBeOnPage(container, history.location.pathname, "login");
     });
   });
 
   describe("visits /", () => {
     it("should take user to Home page with /home as the URL", () => {
-      const history = createMemoryHistory({ initialEntries: ["/"] });
-      const { container } = renderComponent(true, history);
-
+      const { container, history } = renderAuthenticatedComponent("/");
       expectToBeOnPage(container, history.location.pathname, "home");
     });
   });
