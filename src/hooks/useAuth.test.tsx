f27db6f582161b749cce3c69f5fb9a83822a9b22
Naveed Elahi <statix231@hotmail.com>
2020-12-11 10:46:56 +0800

    GCW-3438 Write tests for useAuth hook

---
+++ src/hooks/useAuth.test.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import useAuth from "hooks/useAuth";
+import { render, cleanup } from "@testing-library/react";
+import AuthProvider from "components/AuthProvider/AuthProvider";
+
+interface Auth {
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const setup = (Wrapper: React.FC) => {
+  let auth: Auth | undefined;
+  const TestComponent: React.FC = () => {
+    auth = useAuth();
+    return null;
+  };
+
+  render(<TestComponent />, { wrapper: Wrapper });
+
+  return auth as Auth;
+};
+
+test("should return the correct authentication state", () => {
+  let auth = setup(AuthProvider);
+
+  expect(auth.isAuthenticated).toBe(false);
+
+  cleanup();
+
+  const AuthenticatedAuthProvider: React.FC = ({ children }) => (
+    <AuthProvider initialAuthState={true}>{children}</AuthProvider>
+  );
+  auth = setup(AuthenticatedAuthProvider);
+
+  expect(auth.isAuthenticated).toBe(true);
+});
+
+test("returns a state updater function", () => {
+  const mockSetIsAuthenticated = jest.fn();
+  const mockUseContext = jest.spyOn(React, "useContext").mockReturnValue({
+    isAuthenticated: false,
+    setIsAuthenticated: mockSetIsAuthenticated,
+  });
+
+  const auth = setup(AuthProvider);
+
+  expect(auth.setIsAuthenticated).toBe(mockSetIsAuthenticated);
+
+  mockUseContext.mockRestore();
+});
