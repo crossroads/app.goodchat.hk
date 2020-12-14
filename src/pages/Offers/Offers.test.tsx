9786cf31547788341b256f1b7e6a31a6d688bda7
Naveed Elahi <statix231@hotmail.com>
2020-12-14 15:55:32 +0800

    GCW-3449 Add logout button to Offers header

--- src/pages/Offers/Offers.test.tsx
+++ src/pages/Offers/Offers.test.tsx
@@ -1,8 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Offers from "pages/Offers/Offers";
+import userEvent from "@testing-library/user-event";
+import AuthProvider from "components/AuthProvider/AuthProvider";
+import * as UseAuthModule from "hooks/useAuth/useAuth";
 
 test("renders an offers title", () => {
   const { container } = render(<Offers />);
   expect(container.querySelector("ion-title")).toHaveTextContent(/offers/i);
+});
+
+test("renders a log out button in the header", () => {
+  const { container } = render(<Offers />);
+  expect(container.querySelector("ion-header ion-button")).toHaveTextContent(
+    /log out/i
+  );
+});
+
+test("clicking log out button should call the logout function", () => {
+  const mockLogout = jest.fn();
+  const mockUseAuth = jest.spyOn(UseAuthModule, "default").mockReturnValue({
+    isAuthenticated: true,
+    login: jest.fn(),
+    logout: mockLogout,
+  });
+
+  render(
+    <AuthProvider initialAuthState={true}>
+      <Offers />
+    </AuthProvider>
+  );
+
+  const logoutButton = screen.getByText(/log out/i);
+  userEvent.click(logoutButton);
+
+  expect(mockLogout).toHaveBeenCalledTimes(1);
+
+  mockUseAuth.mockRestore();
 });
--- src/pages/Offers/Offers.tsx
+++ src/pages/Offers/Offers.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonButton,
+  IonButtons,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import useAuth from "hooks/useAuth/useAuth";
 
 const Offers = () => {
+  const { logout } = useAuth();
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Offers</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={logout}>Log out</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
     </IonPage>
