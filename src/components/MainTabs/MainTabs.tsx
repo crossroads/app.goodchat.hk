ecaa1759167b66e21651e51c42427bc64b2157f1
Naveed Elahi <statix231@hotmail.com>
2020-12-18 17:26:34 +0800

    GCW-3454 Create empty tab layout

--- src/components/MainTabs/MainTabs.tsx
+++ src/components/MainTabs/MainTabs.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import Home from "pages/Home/Home";
 import Offers from "pages/Offers/Offers";
 import { Route } from "react-router";
-import { IonRouterOutlet } from "@ionic/react";
+
+import { IonRouterOutlet, IonTabBar, IonTabs } from "@ionic/react";
 
 const MainTabs = () => (
-  <IonRouterOutlet>
-    <Route exact path="/offers">
-      <Offers />
-    </Route>
-    <Route exact path="/home">
-      <Home />
-    </Route>
-  </IonRouterOutlet>
+  <IonTabs>
+    <IonRouterOutlet>
+      <Route exact path="/offers">
+        <Offers />
+      </Route>
+      <Route exact path="/home">
+        <Home />
+      </Route>
+    </IonRouterOutlet>
+    <IonTabBar></IonTabBar>
+  </IonTabs>
 );
 
 export default MainTabs;
