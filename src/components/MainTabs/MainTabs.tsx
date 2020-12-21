c4534fd4feb5f7fc68d333976c579d7d2e80cfa5
Naveed Elahi <statix231@hotmail.com>
2020-12-21 20:42:06 +0800

    GCW-3454 Add Home and Offers icons

--- src/components/MainTabs/MainTabs.tsx
+++ src/components/MainTabs/MainTabs.tsx
@@ -3,12 +3,14 @@ import Home from "pages/Home/Home";
 import Offers from "pages/Offers/Offers";
 import { Route } from "react-router";
 import {
+  IonIcon,
   IonLabel,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonTabs,
 } from "@ionic/react";
+import { home, listOutline } from "ionicons/icons";
 
 const MainTabs = () => (
   <IonTabs>
@@ -22,9 +24,11 @@ const MainTabs = () => (
     </IonRouterOutlet>
     <IonTabBar slot="bottom">
       <IonTabButton tab="home" href="/home">
+        <IonIcon icon={home} />
         <IonLabel>Home</IonLabel>
       </IonTabButton>
       <IonTabButton tab="offers" href="/offers">
+        <IonIcon icon={listOutline} />
         <IonLabel>Offers</IonLabel>
       </IonTabButton>
     </IonTabBar>
