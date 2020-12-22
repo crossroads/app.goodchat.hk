6522edc15e2985e694c8774251a729c25f8ac2dc
Naveed Elahi <statix231@hotmail.com>
2020-12-22 16:07:52 +0800

    GCW-3454 Bottom navbar with tabs (#15)
    * GCW-3454 Create empty tab layout
    
    * GCW-3454 Add ion tab bar to bottom slot
    
    * GCW-3454 Create home tab
    
    * GCW-3454 Use snapshot to assert correctness of tab
    
    * GCW-3454 Test that tabs page renders correctly
    
    * GCW-3454 Add offers tab
    
    * GCW-3454 Update snapshot
    
    * GCW-3454 Update tab page URL matcher format
    
    Referencing: https://ionicframework.com/docs/react/navigation
    
    * GCW-3454 Add Home and Offers icons
    
    * GCW-3454 Explicitly assert on footer correctness
    
    * GCW-3454 Add specs for footer
    
    * GCW-3454 Refactor tests

--- src/components/MainTabs/MainTabs.test.tsx
+++ src/components/MainTabs/MainTabs.test.tsx
@@ -5,7 +5,7 @@ import { expectToBeOnPage } from "test-utils/matchers";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router";
 
-const renderComponent = (initialPath: string) => {
+const renderComponent = (initialPath = "/home") => {
   const history = createMemoryHistory({ initialEntries: [initialPath] });
   return {
     history,
@@ -18,12 +18,65 @@ const renderComponent = (initialPath: string) => {
 };
 
 test("renders without crashing", () => {
-  const { container } = renderComponent("/home");
+  const { container } = renderComponent();
   expect(container).toBeInTheDocument();
 });
 
+test("renders a navbar", () => {
+  const { container } = renderComponent();
+  expect(container.querySelector("ion-tab-bar")).toBeInTheDocument();
+});
+
+test("renders navbar at the bottom of the page", () => {
+  const { container } = renderComponent();
+  expect(container.querySelector("ion-tab-bar")).toHaveAttribute(
+    "slot",
+    "bottom"
+  );
+});
+
+[
+  { tabName: "home", expectedLink: "/home" },
+  { tabName: "offers", expectedLink: "/offers" },
+].forEach(({ tabName, expectedLink }) => {
+  describe(`${tabName} tab`, () => {
+    test("should be rendered", () => {
+      const { container } = renderComponent();
+      expect(
+        container.querySelector(`ion-tab-button[tab=${tabName}]`)
+      ).toBeInTheDocument();
+    });
+
+    test(`should have the appropriate label`, () => {
+      const { container } = renderComponent();
+      expect(
+        container.querySelector(`ion-tab-button[tab=${tabName}] ion-label`)
+      ).toHaveTextContent(new RegExp(tabName, "i"));
+    });
+
+    test(`should have an icon`, () => {
+      const { container } = renderComponent();
+      expect(
+        container.querySelector(`ion-tab-button[tab=${tabName}] ion-icon`)
+      ).toBeInTheDocument();
+    });
+
+    test(`should contain a link to ${expectedLink}`, () => {
+      const { container } = renderComponent();
+      expect(
+        container.querySelector(`ion-tab-button[tab=${tabName}]`)
+      ).toHaveAttribute("href", expectedLink);
+    });
+  });
+});
+
+test("renders footer correctly", () => {
+  const { container } = renderComponent();
+  expect(container.querySelector("ion-tab-bar")).toMatchSnapshot();
+});
+
 test("visiting /home takes user to Home", () => {
-  const { container, history } = renderComponent("/home");
+  const { container, history } = renderComponent();
   expectToBeOnPage(container, history.location.pathname, "home");
 });
 
--- src/components/MainTabs/MainTabs.tsx
+++ src/components/MainTabs/MainTabs.tsx
@@ -2,17 +2,37 @@ import React from "react";
 import Home from "pages/Home/Home";
 import Offers from "pages/Offers/Offers";
 import { Route } from "react-router";
-import { IonRouterOutlet } from "@ionic/react";
+import {
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/react";
+import { home, listOutline } from "ionicons/icons";
 
 const MainTabs = () => (
-  <IonRouterOutlet>
-    <Route exact path="/offers">
-      <Offers />
-    </Route>
-    <Route exact path="/home">
-      <Home />
-    </Route>
-  </IonRouterOutlet>
+  <IonTabs>
+    <IonRouterOutlet>
+      <Route exact path="/:tab(offers)">
+        <Offers />
+      </Route>
+      <Route exact path="/:tab(home)">
+        <Home />
+      </Route>
+    </IonRouterOutlet>
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="home" href="/home">
+        <IonIcon icon={home} />
+        <IonLabel>Home</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="offers" href="/offers">
+        <IonIcon icon={listOutline} />
+        <IonLabel>Offers</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  </IonTabs>
 );
 
 export default MainTabs;
