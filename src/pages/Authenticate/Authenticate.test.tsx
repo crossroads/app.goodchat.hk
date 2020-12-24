73be542dceae7c12b6b582af7f2630b1c08a542c
Naveed Elahi <statix231@hotmail.com>
2020-12-24 09:40:42 +0800

    GCW-3461 Add title

--- src/pages/Authenticate/Authenticate.test.tsx
+++ src/pages/Authenticate/Authenticate.test.tsx
@@ -5,3 +5,10 @@ import Authenticate from "pages/Authenticate/Authenticate";
 test("renders without crashing", () => {
   render(<Authenticate />);
 });
+
+test("renders an authenticate title", () => {
+  const { container } = render(<Authenticate />);
+  expect(container.querySelector("ion-title")).toHaveTextContent(
+    /authenticate/i
+  );
+});
--- src/pages/Authenticate/Authenticate.tsx
+++ src/pages/Authenticate/Authenticate.tsx
@@ -1,5 +1,16 @@
+import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import React from "react";
+
 const Authenticate: React.FC = () => {
-  return null;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Authenticate</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
 };
 
 export default Authenticate;
