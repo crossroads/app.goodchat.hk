9db3157133936b950be42bb8bbd25a0cd80ab4b7
Naveed Elahi <statix231@hotmail.com>
2021-01-11 09:53:48 +0800

    GCW-3417 Refactor tests

--- src/lib/client/GoodCityApiV2Client/index.test.ts
+++ src/lib/client/GoodCityApiV2Client/index.test.ts
@@ -3,17 +3,17 @@ import GoodCityApiV2Client from "./index";
 import sendPinResponse from "./__mocks__/sendPinResponse.js";
 
 describe("sendPin", () => {
-  let spy: jest.SpyInstance;
-  afterEach(() => spy.mockRestore());
+  let mockAxiosPost: jest.SpyInstance;
+  afterEach(() => mockAxiosPost.mockRestore());
 
   it(`should call axios with the correct URL, headers, params, etc`, () => {
     const SEND_PIN_URL = `${process.env.REACT_APP_API_V2_URL}/auth/send_pin`;
-    spy = jest.spyOn(axios, "post");
+    mockAxiosPost = jest.spyOn(axios, "post");
     const mobile = "+85262345678";
 
     GoodCityApiV2Client.sendPin(mobile);
 
-    expect(spy).toHaveBeenCalledWith(
+    expect(mockAxiosPost).toHaveBeenCalledWith(
       SEND_PIN_URL,
       { mobile },
       {
@@ -24,10 +24,10 @@ describe("sendPin", () => {
 
   describe("Successful API response", () => {
     it("it should return the appropriate response", async () => {
-      spy = jest.spyOn(axios, "post").mockResolvedValue(sendPinResponse);
-
+      mockAxiosPost = jest
+        .spyOn(axios, "post")
+        .mockResolvedValue(sendPinResponse);
       const data = await GoodCityApiV2Client.sendPin("+85262345678");
-
       expect(data).toEqual(sendPinResponse);
     });
   });
