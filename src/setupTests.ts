b1da163f029a2065538e6da31e49a88e16d53852
Naveed Elahi <statix231@hotmail.com>
2021-01-15 15:03:30 +0800

    GCW-3478 Mock Service Worker integration (#20)
    * GCW-3478 Install MSW
    
    https://mswjs.io/docs/getting-started/install
    
    * GCW-3478 Create mock server
    
    https://mswjs.io/docs/getting-started/integrate/node#configure-server
    
    * GCW-3478 Set up mock server for tests
    
    https://mswjs.io/docs/getting-started/integrate/node#using-create-react-app
    
    * GCW-3478 Rename server to mockServer + del folder
    
    * GCW-3478 Upgrade to TypeScript 4
    
    Fixes typechecking error with MSW

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import { mockServer } from "mockServer";
+
+beforeAll(() => mockServer.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => mockServer.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => mockServer.close());
