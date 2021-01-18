05f728dda0f6d96b53fa97f900571f8e13754b89
Naveed Elahi <statix231@hotmail.com>
2021-01-18 09:51:34 +0800

    GCW-3417 Test authenticationService methods

--- src/lib/services/AuthenticationService/AuthenticationService.test.ts
+++ src/lib/services/AuthenticationService/AuthenticationService.test.ts
@@ -1,46 +1,53 @@
+import * as GoodCityApiV2ClientModule from "lib/client/GoodCityApiV2Client/GoodCityApiV2Client";
 import AuthenticationService from "lib/services/AuthenticationService/AuthenticationService";
 
-const mockFetch = jest.spyOn(window, "fetch");
-
-afterEach(() => mockFetch.mockClear());
-
-afterAll(() => mockFetch.mockRestore());
-
 describe("sendPin", () => {
-  it(`should call fetch with /auth/send_pin URL and the correct config`, () => {
+  it(`should receive the appropriate response from client`, async () => {
+    const otpAuthKey = "fdsfdsaffdsaklfds";
+    const mockClient = jest
+      .spyOn(GoodCityApiV2ClientModule, "default")
+      .mockResolvedValue({
+        otp_auth_key: otpAuthKey,
+      });
+
     const mobile = "+85262345678";
+    const data = await AuthenticationService.sendPin({ mobile });
 
-    AuthenticationService.sendPin({ mobile });
+    expect(mockClient).toHaveBeenCalledTimes(1);
+    expect(mockClient).toHaveBeenCalledWith("auth/send_pin", { mobile });
 
-    expect(mockFetch).toHaveBeenCalledTimes(1);
-    expect(mockFetch).toHaveBeenCalledWith(
-      `${process.env.REACT_APP_API_V2_URL}/auth/send_pin`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ mobile }),
-      }
-    );
+    expect(data).toEqual({
+      otp_auth_key: otpAuthKey,
+    });
+
+    mockClient.mockRestore();
   });
 });
 
 describe("verify", () => {
-  it("should call fetch with /auth/verify URL and the correct config", () => {
+  it("should receive the appropriate response from client", async () => {
+    const jwtToken = "ejsdfslk3fdsa";
+    const mockClient = jest
+      .spyOn(GoodCityApiV2ClientModule, "default")
+      .mockResolvedValue({
+        jwt_token: jwtToken,
+      });
+
     const otpAuthKey = "sdfscsd2fdsjklf2fs";
     const pin = "1234";
-    AuthenticationService.verify({ pin, otp_auth_key: otpAuthKey });
+    const data = await AuthenticationService.verify({
+      pin,
+      otp_auth_key: otpAuthKey,
+    });
 
-    expect(mockFetch).toHaveBeenCalledTimes(1);
-    expect(mockFetch).toHaveBeenCalledWith(
-      `${process.env.REACT_APP_API_V2_URL}/auth/verify`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          pin: "1234",
-          otp_auth_key: otpAuthKey,
-        }),
-      }
-    );
+    expect(mockClient).toHaveBeenCalledTimes(1);
+    expect(mockClient).toHaveBeenCalledWith("auth/verify", {
+      pin: "1234",
+      otp_auth_key: otpAuthKey,
+    });
+
+    expect(data).toEqual({ jwt_token: jwtToken });
+
+    mockClient.mockRestore();
   });
 });
