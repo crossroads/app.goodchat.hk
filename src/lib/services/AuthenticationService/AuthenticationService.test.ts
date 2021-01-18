6f4e6176e9608e2d6c2b629a767a089b3983992c
Naveed Elahi <statix231@hotmail.com>
2021-01-18 10:27:18 +0800

    GCW-3417 Refactor AuthService tests

--- src/lib/services/AuthenticationService/AuthenticationService.test.ts
+++ src/lib/services/AuthenticationService/AuthenticationService.test.ts
@@ -1,14 +1,19 @@
 import * as GoodCityApiV2ClientModule from "lib/client/GoodCityApiV2Client/GoodCityApiV2Client";
 import AuthenticationService from "lib/services/AuthenticationService/AuthenticationService";
 
+let mockClient: jest.SpyInstance;
+beforeEach(
+  () => (mockClient = jest.spyOn(GoodCityApiV2ClientModule, "default"))
+);
+
+afterEach(() => mockClient.mockRestore());
+
 describe("sendPin", () => {
   it(`should receive the appropriate response from client`, async () => {
     const otpAuthKey = "fdsfdsaffdsaklfds";
-    const mockClient = jest
-      .spyOn(GoodCityApiV2ClientModule, "default")
-      .mockResolvedValue({
-        otp_auth_key: otpAuthKey,
-      });
+    mockClient.mockResolvedValue({
+      otp_auth_key: otpAuthKey,
+    });
 
     const mobile = "+85262345678";
     const data = await AuthenticationService.sendPin({ mobile });
@@ -19,19 +24,15 @@ describe("sendPin", () => {
     expect(data).toEqual({
       otp_auth_key: otpAuthKey,
     });
-
-    mockClient.mockRestore();
   });
 });
 
 describe("verify", () => {
   it("should receive the appropriate response from client", async () => {
     const jwtToken = "ejsdfslk3fdsa";
-    const mockClient = jest
-      .spyOn(GoodCityApiV2ClientModule, "default")
-      .mockResolvedValue({
-        jwt_token: jwtToken,
-      });
+    mockClient.mockResolvedValue({
+      jwt_token: jwtToken,
+    });
 
     const otpAuthKey = "sdfscsd2fdsjklf2fs";
     const pin = "1234";
@@ -42,12 +43,10 @@ describe("verify", () => {
 
     expect(mockClient).toHaveBeenCalledTimes(1);
     expect(mockClient).toHaveBeenCalledWith("auth/verify", {
-      pin: "1234",
+      pin,
       otp_auth_key: otpAuthKey,
     });
 
     expect(data).toEqual({ jwt_token: jwtToken });
-
-    mockClient.mockRestore();
   });
 });
