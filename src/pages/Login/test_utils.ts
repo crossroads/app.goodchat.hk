ec65e68ca613829797d5ce649f4753dc770b5831
Naveed Elahi <statix231@hotmail.com>
2021-01-22 16:31:51 +0800

    GCW-3462 Modify ion-input selection query
    It assumes implementation details

--- src/pages/Login/test_utils.ts
+++ src/pages/Login/test_utils.ts
@@ -2,7 +2,7 @@ import { act } from "@testing-library/react";
 import userEvent, { TargetElement } from "@testing-library/user-event";
 
 function fillInput(container: HTMLElement, phoneInput: string) {
-  const input = container.querySelector("ion-item > ion-input");
+  const input = container.querySelector("ion-input");
   act(() => {
     input!.dispatchEvent(
       new CustomEvent("ionChange", {
