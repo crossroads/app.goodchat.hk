ab829a61634739f04f4240a50fdab5d4fd8d0371
Naveed Elahi <statix231@hotmail.com>
2021-01-26 12:22:48 +0800

    GCW-3483 Integrate ionic react test utils (#23)
    * GCW-3438 Install Ionic React test utils
    
    * GCW-3438 Replace custom event dispatching

--- src/pages/Login/test_utils.ts
+++ src/pages/Login/test_utils.ts
@@ -1,15 +1,9 @@
-import { act } from "@testing-library/react";
 import userEvent, { TargetElement } from "@testing-library/user-event";
+import { ionFireEvent } from "@ionic/react-test-utils";
 
 function fillIonInput(container: HTMLElement, phoneInput: string) {
   const input = container.querySelector("ion-input");
-  act(() => {
-    input!.dispatchEvent(
-      new CustomEvent("ionChange", {
-        detail: { value: phoneInput },
-      })
-    );
-  });
+  ionFireEvent.ionChange(input!, phoneInput);
 }
 function clickButton(container: HTMLElement) {
   const button = container.querySelector("ion-button");
