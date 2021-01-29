afb26f538cbe09416abd7599f4106bacdf7cbae9
Naveed Elahi <statix231@hotmail.com>
2021-01-29 15:46:56 +0800

    GCW-3486 Return service responses

--- src/lib/services/AuthenticationService/AuthenticationService.ts
+++ src/lib/services/AuthenticationService/AuthenticationService.ts
@@ -7,12 +7,13 @@ import {
   VerifyResponse,
 } from "lib/services/AuthenticationService/types";
 
-async function sendPin(body: SendPinBody): Promise<void> {
+async function sendPin(body: SendPinBody): Promise<SendPinResponse> {
   const response: SendPinResponse = await client.post("auth/send_pin", body);
   localStorage.setItem(OTP_AUTH_KEY, response.otp_auth_key);
+  return response;
 }
 
-async function authenticate(pin: string): Promise<void> {
+async function authenticate(pin: string): Promise<VerifyResponse> {
   const body: VerifyBody = {
     otp_auth_key: localStorage.getItem(OTP_AUTH_KEY) ?? "",
     pin,
@@ -20,6 +21,7 @@ async function authenticate(pin: string): Promise<void> {
   const response: VerifyResponse = await client.post("auth/verify", body);
   localStorage.setItem(GC_API_TOKEN, response.jwt_token);
   localStorage.removeItem(OTP_AUTH_KEY);
+  return response;
 }
 
 function logout() {
