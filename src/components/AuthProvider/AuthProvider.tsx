b04b1ee891b767ac74fcc7d05243b04853e103fc
Naveed Elahi <statix231@hotmail.com>
2021-03-05 16:21:17 +0800

    GCW-3525 Set auth state based on props if provided

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
 import AuthContext from "context/AuthContext";
 import AuthenticationService from "lib/services/AuthenticationService/AuthenticationService";
-
-export const computeAuthState = (initialAuthState: boolean) => {
-  return Boolean(AuthenticationService.isAuthenticated() ?? initialAuthState);
-};
 interface Props {
   initialAuthState?: boolean;
 }
-const AuthProvider: React.FC<Props> = ({
-  children,
-  initialAuthState = false,
-}) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(() =>
-    computeAuthState(initialAuthState)
+const AuthProvider: React.FC<Props> = ({ children, initialAuthState }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => initialAuthState ?? AuthenticationService.isAuthenticated()
   );
 
   return (
