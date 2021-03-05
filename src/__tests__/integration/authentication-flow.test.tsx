8196e23518b450037819e8b17b4d219f4538c093
Naveed Elahi <statix231@hotmail.com>
2021-03-05 11:03:37 +0800

    GCW-3525 Use generic localStorage cleanup

--- src/__tests__/integration/authentication-flow.test.tsx
+++ src/__tests__/integration/authentication-flow.test.tsx
@@ -7,7 +7,6 @@ import AuthProvider from "components/AuthProvider/AuthProvider";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router";
 import MainRouter from "components/MainRouter/MainRouter";
-import { GC_API_TOKEN } from "test-utils/config/localStorageKeys";
 import { ionFireEvent } from "@ionic/react-test-utils";
 import { mockServer } from "mockServer";
 import { rest } from "msw";
@@ -29,6 +28,8 @@ beforeAll(() => {
   );
   mockServer.listen({ onUnhandledRequest: "error" });
 });
+
+afterEach(() => localStorage.clear());
 
 afterAll(() => mockServer.close());
 
@@ -96,7 +97,4 @@ test("Unauthenticated user redirected to login is redirected back to that page a
   await wait(() =>
     expectToBeOnPage(container, history.location.pathname, "offers")
   );
-
-  // cleanup
-  localStorage.removeItem(GC_API_TOKEN);
 });
