a98fa58e631ea0df3c28c23ec6c3b6cf45ffb6fc
Naveed Elahi <statix231@hotmail.com>
2021-03-11 14:53:36 +0800

    GCW-3451 Remove false positive from client test

--- src/lib/client/client.test.ts
+++ src/lib/client/client.test.ts
@@ -10,20 +10,18 @@ afterEach(() => mockServer.resetHandlers());
 afterAll(() => mockServer.close());
 
 test("should call GoodCity API V2 correctly", () => {
-  const otpAuthKey = "sfdsfasxfds";
   mockServer.use(
-    rest.post(
-      `${process.env.REACT_APP_API_V2_URL}/auth/send_pin`,
-      (_, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ otp_auth_key: otpAuthKey }));
+    rest.post<{ value: string }>(
+      `${process.env.REACT_APP_API_V2_URL}/endpoint`,
+      (req, res, ctx) => {
+        if (req.body.value === undefined) return res(ctx.status(400));
+        return res(ctx.status(200), ctx.json({ success: true }));
       }
     )
   );
 
-  return expect(
-    client.post("auth/send_pin", { mobile: "+85291111111" })
-  ).resolves.toEqual({
-    otp_auth_key: otpAuthKey,
+  return expect(client.post("endpoint", { value: "1234" })).resolves.toEqual({
+    success: true,
   });
 });
 
