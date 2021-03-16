0716097d84dac652f00796607b1002dfb05ee861
Naveed Elahi <statix231@hotmail.com>
2021-03-16 16:08:23 +0800

    GCW-3546 Append auth headers on every request

--- src/lib/HasuraClient.ts
+++ src/lib/HasuraClient.ts
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const HASURA_TOKEN_KEY = "hasura_token";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HASURA_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(HASURA_TOKEN_KEY);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_HASURA_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
