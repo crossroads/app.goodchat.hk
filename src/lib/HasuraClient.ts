d1a975b4e659601d3fd7bceb280f6913da001eaf
Naveed Elahi <statix231@hotmail.com>
2021-03-16 17:36:05 +0800

    GCW-3546 Basic Hasura client (#34)
    * GCW-3546 Install apollo client + graphql
    
    * GCW-3546 Create env for hasura endpoint url
    
    * GCW-3546 Create basic apollo hasura client
    
    * GCW-3546 Wrap app with ApolloProvider
    
    Grants access to graphQL queries
    
    * GCW-3546 Append auth headers on every request
    
    * GCW-3546 Store hasura token in memory

---
+++ src/lib/HasuraClient.ts
@@ -0,0 +1,24 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import AuthenticationService from "lib/services/AuthenticationService/AuthenticationService";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HASURA_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = AuthenticationService.getHasuraToken();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
