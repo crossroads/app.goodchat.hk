b162afcf3894561708a3f731389fa52f5b8aa7cd
Naveed Elahi <statix231@hotmail.com>
2021-04-27 15:43:40 +0800

    GCW-3601 Refactor offers and home tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,36 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Home from "pages/Home/Home";
-import AuthProvider from "components/AuthProvider/AuthProvider";
-import userEvent, { TargetElement } from "@testing-library/user-event";
-import useAuth from "hooks/useAuth/useAuth";
 
-const TestComponent: React.FC = () => {
-  const { isAuthenticated } = useAuth();
+import { expectToRenderHeaderCorrectly } from "test-utils/matchers";
 
-  if (isAuthenticated)
-    return (
-      <div data-testid="home">
-        <Home />
-      </div>
-    );
-  return null;
-};
-
-test("renders correctly", () => {
+test("renders without crashing", () => {
   const { container } = render(<Home />);
-  expect(container).toMatchSnapshot();
+  expect(container).toBeInTheDocument();
 });
 
-test("clicking log out button should log user out", () => {
-  render(
-    <AuthProvider initialAuthState={true}>
-      <TestComponent />
-    </AuthProvider>
-  );
-
-  const logoutButton = screen.getByText(/log out/i);
-  userEvent.click(logoutButton as TargetElement);
-
-  expect(screen.queryByTestId(/home/i)).not.toBeInTheDocument();
-});
+expectToRenderHeaderCorrectly(Home, "Home");
