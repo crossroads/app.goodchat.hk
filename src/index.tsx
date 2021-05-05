08b1a4272120baa0f17647c85d9c3ee68c6a0755
Naveed Elahi <statix231@hotmail.com>
2021-05-05 16:30:32 +0800

    GCW-3622 Revert env checking logic
    Our build would still pass even if we check and throw error
    Better to throw before app even has chance to be run, built or tested

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import App from "App";
 import * as serviceWorker from "serviceWorker";
 
-if (
-  !process.env.REACT_APP_API_V2_URL ||
-  !process.env.REACT_APP_HASURA_URL ||
-  !process.env.REACT_APP_GOODCHAT_URL
-) {
-  throw new Error("Missing environment variables!");
-}
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
