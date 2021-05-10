b8de00b4e4f8c7ad0022badcbdc205dce24b6f76
Naveed Elahi <statix231@hotmail.com>
2021-05-10 16:13:11 +0800

    GCW-3632 Create chat page header

--- src/pages/Chat/Chat.test.tsx
+++ src/pages/Chat/Chat.test.tsx
@@ -1,7 +1,18 @@
 import { render } from "@testing-library/react";
 import Chat from "pages/Chat/Chat";
+import { pageHeader } from "test-utils/matchers";
 
 test("renders without crashing", () => {
   const { container } = render(<Chat />);
   expect(container).toBeInTheDocument();
 });
+
+describe(
+  "Chat page header",
+  pageHeader({
+    title: "Chat",
+    privatePage: true,
+    withBackButton: true,
+    element: <Chat />,
+  })
+);
--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,5 +1,33 @@
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import useAuth from "hooks/useAuth/useAuth";
+import React from "react";
+
 const Chat: React.FC = () => {
-  return null;
+  const { logout } = useAuth();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton />
+          </IonButtons>
+          <IonTitle>Chat</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={logout}>Log out</IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
 };
 
 export default Chat;
