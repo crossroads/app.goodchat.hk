875b39cc066b02d56ee8c89eda85b092774a5ec9
Naveed Elahi <statix231@hotmail.com>
2021-06-03 09:51:54 +0800

    GCW-3650 Refactor main router tests

--- src/components/MainRouter/MainRouter.test.tsx
+++ src/components/MainRouter/MainRouter.test.tsx
@@ -30,24 +30,24 @@ describe("Unauthenticated User", () => {
   const renderUnauthenticatedComponent = renderComponent(false);
 
   [
-    { initialPath: "/home", expectedPage: "login" },
-    { initialPath: "/chats", expectedPage: "login" },
-    { initialPath: "/chats/1", expectedPage: "login" },
-    { initialPath: "/offers", expectedPage: "login" },
-    { initialPath: "/login", expectedPage: "login" },
-    { initialPath: "/authenticate", expectedPage: "authenticate" },
-    { initialPath: "/", expectedPage: "login" },
-    { initialPath: "/bad-route", expectedPage: "login" },
-    { initialPath: "/home/bad-route", expectedPage: "login" },
-    { initialPath: "/chats/bad-route", expectedPage: "login" },
-    { initialPath: "/chats/1", expectedPage: "login" },
-    { initialPath: "/offers/bad-route", expectedPage: "login" },
-    { initialPath: "/login/bad-route", expectedPage: "login" },
-    { initialPath: "/authenticate/bad-route", expectedPage: "login" },
-  ].map(({ initialPath, expectedPage }) => {
-    it(`visiting ${initialPath} should be taken to ${expectedPage}`, async () => {
+    { initialPath: "/home",                    expectedPath: "/login" },
+    { initialPath: "/chats",                   expectedPath: "/login" },
+    { initialPath: "/chats/1",                 expectedPath: "/login" },
+    { initialPath: "/offers",                  expectedPath: "/login" },
+    { initialPath: "/login",                   expectedPath: "/login" },
+    { initialPath: "/authenticate",            expectedPath: "/authenticate" },
+    { initialPath: "/",                        expectedPath: "/login" },
+    { initialPath: "/bad-route",               expectedPath: "/login" },
+    { initialPath: "/home/bad-route",          expectedPath: "/login" },
+    { initialPath: "/chats/bad-route",         expectedPath: "/login" },
+    { initialPath: "/chats/1",                 expectedPath: "/login" },
+    { initialPath: "/offers/bad-route",        expectedPath: "/login" },
+    { initialPath: "/login/bad-route",         expectedPath: "/login" },
+    { initialPath: "/authenticate/bad-route",  expectedPath: "/login" },
+  ].map(({ initialPath, expectedPath }) => {
+    it(`visiting ${initialPath} should be taken to ${expectedPath}`, async () => {
       const { history } = await renderUnauthenticatedComponent(initialPath);
-      expect(history.location.pathname).toEqual(`/${expectedPage}`);
+      expect(history.location.pathname).toEqual(expectedPath);
     });
   });
 });
@@ -56,22 +56,22 @@ describe("Authenticated User", () => {
   const renderAuthenticatedComponent = renderComponent(true);
 
   [
-    { initialPath: "/home", expectedPage: "home" },
-    { initialPath: "/chats", expectedPage: "chats" },
-    { initialPath: "/chats/1", expectedPage: "chat", expectedPath: "/chats/1" },
-    { initialPath: "/offers", expectedPage: "offers" },
-    { initialPath: "/login", expectedPage: "login" },
-    { initialPath: "/authenticate", expectedPage: "authenticate" },
-    { initialPath: "/", expectedPage: "home" },
-    { initialPath: "/bad-route", expectedPage: "home" },
-    { initialPath: "/home/bad-route", expectedPage: "home" },
-    { initialPath: "/offers/bad-route", expectedPage: "home" },
-    { initialPath: "/login/bad-route", expectedPage: "home" },
-    { initialPath: "/authenticate/bad-route", expectedPage: "home" },
-  ].map(({ initialPath, expectedPage, expectedPath }) => {
-    it(`visiting ${initialPath} should be taken to ${expectedPage}`, async () => {
+    { initialPath: "/home",                    expectedPath: "/home" },
+    { initialPath: "/chats",                   expectedPath: "/chats" },
+    { initialPath: "/chats/1",                 expectedPath: "/chats/1"},
+    { initialPath: "/offers",                  expectedPath: "/offers" },
+    { initialPath: "/login",                   expectedPath: "/login" },
+    { initialPath: "/authenticate",            expectedPath: "/authenticate" },
+    { initialPath: "/",                        expectedPath: "/home" },
+    { initialPath: "/bad-route",               expectedPath: "/home" },
+    { initialPath: "/home/bad-route",          expectedPath: "/home" },
+    { initialPath: "/offers/bad-route",        expectedPath: "/home" },
+    { initialPath: "/login/bad-route",         expectedPath: "/home" },
+    { initialPath: "/authenticate/bad-route",  expectedPath: "/home" },
+  ].map(({ initialPath, expectedPath }) => {
+    it(`visiting ${initialPath} should be taken to ${expectedPath}`, async () => {
       const { history } = await renderAuthenticatedComponent(initialPath);
-      expect(history.location.pathname).toEqual(expectedPath ?? `/${expectedPage}`);
+      expect(history.location.pathname).toEqual(expectedPath);
     });
   });
 });
